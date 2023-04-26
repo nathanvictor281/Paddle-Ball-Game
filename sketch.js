ece996275a3e11d61bf28a720eed0077b9574529
Nv <130412927+nathanvictor281@users.noreply.github.com>
2023-04-25 21:46:45 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -6,8 +6,8 @@ function setup() {
   aiY = height / 2;
   ballX = width / 2;
   ballY = height / 2;
-  ballXSpeed = 7;
-  ballYSpeed = 7;
+  ballXSpeed = 12;
+  ballYSpeed = 12;
   playerScore = 0;
   aiScore = 0;
 }
