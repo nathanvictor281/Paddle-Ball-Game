61a79966e2a7bf7683f0f653582d49c21299ebd4
Nv <130412927+nathanvictor281@users.noreply.github.com>
2023-09-21 21:16:45 -0700

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,4 +1,5 @@
 let playerY, aiY, ballX, ballY, ballXSpeed, ballYSpeed, playerScore, aiScore;
+let difficultyLevel = "medium"; // Change this to "easy" or "hard" for different difficulties
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
@@ -6,68 +7,66 @@ function setup() {
   aiY = height / 2;
   ballX = width / 2;
   ballY = height / 2;
-  ballXSpeed = 8;
-  ballYSpeed = 8;
   playerScore = 0;
   aiScore = 0;
+  setDifficulty(difficultyLevel);
 }
 
 function draw() {
   background(0);
-  
+
   // Draw middle line
   stroke(255);
   line(width / 2, 0, width / 2, height);
-  
+
   // Draw paddles
   rect(20, playerY - 40, 10, 80);
   rect(width - 30, aiY - 40, 10, 80);
-  
+
   // Update ball position
   ballX += ballXSpeed;
   ballY += ballYSpeed;
-  
+
   // Check ball collision with walls
-  if(ballY < 0 || ballY > height) {
+  if (ballY < 0 || ballY > height) {
     ballYSpeed *= -1;
   }
-  
+
   // Check ball collision with paddles
-  if(ballX < 30 && ballY > playerY - 40 && ballY < playerY + 40) {
+  if (ballX < 30 && ballY > playerY - 40 && ballY < playerY + 40) {
     ballXSpeed *= -1.1;
     ballYSpeed *= 1.1;
   }
-  
-  if(ballX > width - 30 && ballY > aiY - 40 && ballY < aiY + 40) {
+
+  if (ballX > width - 30 && ballY > aiY - 40 && ballY < aiY + 40) {
     ballXSpeed *= -1.1;
     ballYSpeed *= 1.1;
   }
-  
+
   // Check ball collision with score walls
-  if(ballX < 0) {
+  if (ballX < 0) {
     aiScore++;
     resetBall();
   }
-  
-  if(ballX > width) {
+
+  if (ballX > width) {
     playerScore++;
     resetBall();
   }
-  
+
   // Draw ball
   ellipse(ballX, ballY, 20);
-  
+
   // Draw score
   textAlign(CENTER);
   textSize(32);
   fill(255);
   text(playerScore + " - " + aiScore, width / 2, 40);
-  
-  // Move AI paddle
-// In draw(), change the AI's y-position update to a slower rate (e.g., only update every other frame):
-if (frameCount % 6 === 0) {
-  aiY = ballY;
-}
+
+  // Move AI paddle based on difficulty
+  if (frameCount % aiReactionRate === 0) {
+    aiY = ballY;
+  }
 
   // Check if there is a winner
   if (playerScore === 7) {
@@ -94,4 +93,24 @@ function resetBall() {
 
 function mouseMoved() {
   playerY = mouseY;
+}
+
+function setDifficulty(level) {
+  switch (level) {
+    case "easy":
+      aiReactionRate = 10; // Slower reaction time
+      ballXSpeed = 4; // Slower ball speed
+      ballYSpeed = 4;
+      break;
+    case "medium":
+      aiReactionRate = 6; // Medium reaction time
+      ballXSpeed = 5; // Medium ball speed
+      ballYSpeed = 5;
+      break;
+    case "hard":
+      aiReactionRate = 3; // Faster reaction time
+      ballXSpeed = 6; // Faster ball speed
+      ballYSpeed = 6;
+      break;
+  }
 }
